53a1a859fed84badcf6d20a87faef45c8ce434f1
Ryan Freckleton <ryan.freckleton@gmail.com>
2018-04-09 13:41:38 -0600

    Update columns

--- kiloblog.py
+++ kiloblog.py
@@ -5,10 +5,12 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 db = SQLAlchemy(app)
 
+
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Colun(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120, unique=True, nullable=False))
+    title = db.Colun(db.String(80), nullable=False)
+    content = db.Column(db.Text(120, nullable=False))
+    pub_time = db.Column(db.Date())
 
     def __repr__(self):
         return '<User %r>' % self.username
