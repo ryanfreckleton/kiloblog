0b89e17c56680c91df763b186b6c1849381f3a1d
Ryan Freckleton <ryan.freckleton@gmail.com>
2018-04-09 13:36:32 -0600

    Simplify

--- kiloblog.py
+++ kiloblog.py
@@ -1,8 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+db = SQLAlchemy(app)
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Colun(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120, unique=True, nullable=False))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return render_template('index.html', blog_title="A Byte Sized Blog")
