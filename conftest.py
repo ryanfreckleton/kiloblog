2f90c3d28d457965a57182f3c4d9d9b6d63ce6d4
Ryan Freckleton <ryan.freckleton@gmail.com>
2018-04-10 15:48:58 -0600

    Start work on tests.

--- conftest.py
+++ conftest.py
@@ -4,4 +4,7 @@ import kiloblog
 
 @pytest.fixture
 def app():
+    kiloblog.app.testing = True
+    kiloblog.app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://'
+    kiloblog.db.create_all()
     return kiloblog.app
--- test_kiloblog.py
+++ test_kiloblog.py
@@ -1,6 +1,33 @@
-class TestIndex:
-    def test_get(self, client):
-        assert client.get('/').status == '200 OK'
+from hypothesis import given, strategies as st
+
+
+class TestGeneralBehavior:
+    """
+    Test general properties of the application.
+    """
+    @given(st.text())
+    def test_get(self, client, text):
+        """
+        When an arbitrary URL is tried with GET, it doesn't return a 500 error.
+        """
+        assert client.get('/'+text).status_code != 500
+
+    @given(st.text())
+    def test_post(self, client, text):
+        """
+        When an arbitrary URL is tried with POST, it doesn't return a 500 error.
+        """
+        assert client.post('/'+text).status_code != 500
+
+
+class TestLogin:
+    @given(st.text(), st.text())
+    def test_username_password(self, client, username, password):
+        """
+        The login prompt should return a 200 OK status no matter what.
+        """
+        assert client.post('/login',
+                           data=dict(username=username, password=password, follow_redirects=True)).status_code == 200
 
 # Invariants
 # - Don't ever throw a 500 exception
