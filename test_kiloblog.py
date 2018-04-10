3f53db50581aee088dd03df3abc372b14a70776e
Ryan Freckleton <ryan.freckleton@gmail.com>
2018-04-10 11:41:51 -0600

    Removing old tests

--- test_kiloblog.py
+++ test_kiloblog.py
@@ -1,5 +1,3 @@
-import datetime
-
 import pytest
 
 import kiloblog
@@ -19,28 +17,3 @@ def client():
 class TestIndex:
     def test_get(self, client):
         assert client.get('/').status == '200 OK'
-
-
-class TestCreatePost:
-    post = kiloblog.Post(title='new blog title', content='content goes here', pub_date=datetime.date(2000, 1, 1))
-
-    def test_new_post(self):
-
-        actions = kiloblog.make_post({'title': 'new blog title', 'content': 'content goes here',
-                                      'pub_date': datetime.date(2000, 1, 1)})
-        assert actions == [kiloblog.Save(self.post), kiloblog.RedirectToPost(self.post)]
-
-    def test_save(self):
-        post = kiloblog.Post(title="new blog title", content="content goes here", pub_date=datetime.date(2000, 1, 1))
-        save = kiloblog.Save(post)
-        save.do()
-        post_from_db = kiloblog.PostModel.query.get(1)
-        assert kiloblog.PostModel.query.count() == 1
-        assert post_from_db.title == post.title
-        assert post_from_db.content == post.content
-        assert post_from_db.pub_date == datetime.date(2000, 1, 1)
-
-    def test_redirect_to_post(self):
-        with kiloblog.app.test_request_context():
-            redirect = kiloblog.RedirectToPost(self.post).do()
-            assert redirect.headers['Location'] == '/2000/1/1/new-blog-title'
